82bf8b05dd523edcc2113f7425a58a37d5a3f9a9
Jalil Nouri <nouri.6412@gmail.com>
2022-02-12 15:39:36 +0330

    1

--- template-parts/footer/footer-copy.php
+++ template-parts/footer/footer-copy.php
@@ -5,8 +5,8 @@ $data = get_field("footer-copy", 'option');
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <span> © کپی رایت 1400 با <i class="fa fa-heart m-lr5 text-red heart"></i>
-                        <a href="http://mobi-tel.ca/">designed by mobi-tel.ca </a> </span>
+                    <div style="direction: ltr;"> ©copyright <i class="fa fa-heart m-lr5 text-red heart"></i>
+                        <a href="http://mobi-tel.ca/">designed by mobi-tel.ca </a> </div>
                 </div>
             </div>
         </div>
