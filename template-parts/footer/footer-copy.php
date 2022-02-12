9a677146dad01eff0767cd931a09ed9e881de8ed
Jalil Nouri <nouri.6412@gmail.com>
2022-02-12 15:34:33 +0330

    11

--- template-parts/footer/footer-copy.php
+++ template-parts/footer/footer-copy.php
@@ -6,7 +6,7 @@ $data = get_field("footer-copy", 'option');
             <div class="row">
                 <div class="col-lg-12 text-center">
                     <span> © کپی رایت 1400 با <i class="fa fa-heart m-lr5 text-red heart"></i>
-                        <a href="#"><?php  echo 'Karyabi'; ?> </a> همه حقوق محفوظ است.</span>
+                        <a href="http://mobi-tel.ca/">designed by mobi-tel.ca </a> </span>
                 </div>
             </div>
         </div>
