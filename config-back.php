6b9e9333407d42eee1f4cfda6976573c5aca8286
Jalil Nouri <nouri.6412@gmail.com>
2022-02-16 13:06:59 +0330

    1

---
+++ config-back.php
@@ -0,0 +1,10 @@
+// define( 'DB_NAME', 'blitdani_karyabi' );
+
+// /** MySQL database username */
+// define( 'DB_USER', 'blitdani_crm' );
+
+// /** MySQL database password */
+// define( 'DB_PASSWORD', 'fl(QdXZ~JDVH' );
+
+// /** MySQL hostname */
+// define( 'DB_HOST', '185.159.153.76' );
