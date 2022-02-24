7519d0e5b6c3498c0d1d224db07dabb566cdeb4f
Home Pc <nouri.6412@gmail.com>
2022-02-24 15:42:51 +0330

    8888

--- bot.php
+++ bot.php
@@ -1,11 +1,24 @@
 <?php
-set_time_limit ( 60 );
+set_time_limit(60);
+$ex = date('Y-m-d H:i:s');
+
+$myfile = fopen("bot.txt", "w") or die("Unable to open file!");
+$txt = $ex;
+fwrite($myfile, $txt);
+fclose($myfile);
+
 function task()
 {
     $str = file_get_contents('http://wordpress-themes.ir/bot');
 }
 
 for ($i = 0; $i < 30; $i++) {
+    $myfile = fopen("bot.txt", "r") or die("Unable to open file!");
+    $ex1 = fgets($myfile);
+    fclose($myfile);
+    if ($ex != $ex1) {
+        break;
+    }
     sleep(3);
     task();
 }
